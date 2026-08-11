210c1ecd69103377d125835c4671677063fa3323
jeidy11 <martinezjeidy40@gmail.com>
2026-08-11 12:53:43 -0500

    clase1-programacion2

---
+++ carros/Assets/Cars/CarSo.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New car", menuName = "Car/NewCar")]
+public class CarSo : ScriptableObject 
+
+{
+    public float speed;
+    public float brakeForce;
+    public float angle;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+}
